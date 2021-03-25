6bbb4977f507f302a93dc1f050798ac05adf4d49
Alok Singh <adb.singh143@gmail.com>
2021-03-26 03:35:13 +0530

    slider

---
+++ src/components/landingPage/Banner.js
@@ -0,0 +1,17 @@
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+
+export default function slider() {
+    return(
+        <AwesomeSlider>
+            <div><img src={"https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg"}/> </div>
+            <div><img src={"https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg"}/> </div>
+            <div><img src={"https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg"}/> </div>
+            <div><img src={"https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg"}/> </div>
+            <div><img src={"https://collider.com/wp-content/uploads/inception_movie_poster_banner_01.jpg"}/> </div>
+
+        </AwesomeSlider>
+    )
+}
+
+
